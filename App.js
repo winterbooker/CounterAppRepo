062b66210cd1e0409d44b73aaf4c504e60486700
山下拓 <yamashitataku@taku.local>
2020-08-28 20:13:28 +0900

    hide firebase certificate

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import firebase from 'firebase';
 
 import CounterListScreen from './src/screens/CounterListScreen';
 import CounterDetailScreen from './src/screens/CounterDetailScreen';
 import CounterEditScreen from './src/screens/CounterEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
+
+import ENV from './env.json';
+
+const firebaseConfig = {
+  apiKey:            ENV.FIREBASE_API_KEY,
+  authDomain:        ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL:       ENV.FIREBASE_DB_URL,
+  projectId:         ENV.FIREBASE_PRJ_ID,
+  storageBucket:     ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+};
+firebase.initializeApp(firebaseConfig)
 
 const App = createStackNavigator({
   Login:         { screen: LoginScreen },
